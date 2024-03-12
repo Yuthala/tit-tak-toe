2f5ae41c3bface56798da705513cff732baf1cde
Yuthala <dinapolousova@gmail.com>
2024-03-12 21:42:12 +0300

    tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: {
+    relative: true,
+    files: [
+      './pages/**/*.{html,js,jsx,ts,tsx}',
+    ],
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
