e43807cd12ab6358985c83b9f41600413d7cbd18
Yuthala <dinapolousova@gmail.com>
2024-03-14 10:00:25 +0300

    install eslint and prettier

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "16e68d52c4694676"; }
+/******/ 		__webpack_require__.h = function() { return "daf25cf94461c520"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/test.jsx
+++ pages/test.jsx
@@ -1,3 +1,5 @@
+import "tailwindcss/tailwind.css";
+
 export default function TestPage() {
 	return <div className="text-2xl text-red-800">Text on the page</div>
 }
